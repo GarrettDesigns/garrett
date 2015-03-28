9a2d736eb356baad302a7ed71aa63ac29acdc315
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-03-28 13:08:00 -0500

    add gallery and made responsive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,24 +11,28 @@ angular.module('garrettApp')
   .controller('MainController', function ($scope) {
   	$scope.portfolio = [
   		{
-  			image: 'http://dummyimage.com/400x250/4d494d/686a82.gif&text=placeholder+image',
+  			image: 'images/redhawkcasino_03.jpg',
   			description: 'This is the first item'
   		},
   		{
-  			image: 'http://dummyimage.com/400x250/4d494d/686a82.gif&text=placeholder+image',
+  			image: 'images/redhawkcasino_03.jpg',
   			description: 'This is the second item'
   		},
   		{
-  			image: 'http://dummyimage.com/400x250/4d494d/686a82.gif&text=placeholder+image',
+  			image: 'images/redhawkcasino_03.jpg',
   			description: 'This is the third item'
   		},
   		{
-  			image: 'http://dummyimage.com/400x250/4d494d/686a82.gif&text=placeholder+image',
+  			image: 'images/redhawkcasino_03.jpg',
   			description: 'This is the fourth item'
   		},
+      {
+        image: 'images/redhawkcasino_03.jpg',
+        description: 'This is the fifth item'
+      },
   		{
-  			image: 'http://dummyimage.com/400x250/4d494d/686a82.gif&text=placeholder+image',
-  			description: 'This is the fifth item'
+  			image: 'images/redhawkcasino_03.jpg',
+  			description: 'This is the sixth item'
   		}
   	];
   });
