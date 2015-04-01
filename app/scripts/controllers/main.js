68863e1c090e7e2378e35f448eae63d47adaa094
Garrett <garrett.kucinski@gmail.com>
2015-04-01 12:44:50 -0500

    made gallery hover states, added real images, added mixins for buttons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,40 +11,58 @@ angular.module('garrettApp')
   .controller('MainController', function ($scope) {
   	$scope.portfolio = [
   		{
-  			image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-  			description: 'This is the first item'
+  			title: 'Project Title',
+  			image: '../images/redhawkcasino_03.jpg',
+  			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus rerum, facere. Odit suscipit saepe quas nisi dolore tenetur, repellendus numquam autem vitae ducimus, aspernatur, architecto debitis animi possimus itaque laudantium.',
+  			show: false
   		},
   		{
-  			image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-  			description: 'This is the second item'
+  			title: 'Project Title',
+  			image: '../images/tenthingsLg.jpg',
+  			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet consectetur, sunt iste cupiditate dolor recusandae officia assumenda, non et officiis perspiciatis! Dolor officiis veritatis, sunt suscipit non ullam asperiores facilis!',
+  			show: false
   		},
   		{
-  			image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-  			description: 'This is the third item'
+  			title: 'Project Title',
+  			image: '../images/grandcentralLg.jpg',
+  			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore corporis cupiditate ratione repellat, aut praesentium dolorem ut ipsam quo temporibus veniam aliquid ad possimus sed unde, perferendis magni velit beatae.',
+  			show: false
   		},
   		{
-  			image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-  			description: 'This is the fourth item'
+  			title: 'Project Title',
+  			image: '../images/inrule.png',
+  			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, maiores assumenda dolores omnis quisquam aut ipsum accusantium mollitia eum neque tempore ut sunt nobis nostrum maxime nesciunt odit porro quos.',
+  			show: false
   		},
       {
-        image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-        description: 'This is the fifth item'
+      	title: 'Project Title',
+        image: '../images/northshore.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aspernatur pariatur, tenetur repellat voluptas? Nobis, id rerum ullam doloremque maiores, architecto? Illo nulla accusamus aspernatur officiis necessitatibus facilis facere praesentium.',
+        show: false
       },
   		{
-  			image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-  			description: 'This is the sixth item'
+  			title: 'Project Title',
+  			image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+  			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quod, obcaecati doloremque nobis. Nihil porro odit libero, temporibus unde ipsum culpa veritatis, magni voluptatibus, impedit, necessitatibus ratione nisi inventore est.',
+  			show: false
   		},
       {
-        image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-        description: 'This is the seventh item'
+      	title: 'Project Title',
+        image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus numquam dolorem quos repellat nisi sed iste. Rem unde iste quibusdam itaque. Dolorem nesciunt vero quis deserunt ea, consectetur eligendi magnam.',
+        show: false
       },
       {
-        image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-        description: 'This is the eighth item'
+      	title: 'Project Title',
+        image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique amet debitis eligendi consequuntur maiores enim doloremque iste fugiat delectus quisquam modi nemo omnis repellat neque rem soluta, aspernatur, atque recusandae?',
+        show: false
       },
       {
-        image: 'http://dummyimage.com/800x800/4d494d/686a82.gif&text=placeholder+image',
-        description: 'This is the ninth item'
+      	title: 'Project Title',
+        image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga dolor est obcaecati maxime animi distinctio quia quo recusandae magni earum incidunt, quibusdam repellat nobis. Minus delectus corporis culpa eos accusamus.',
+        show: false
       }
   	];
   });
