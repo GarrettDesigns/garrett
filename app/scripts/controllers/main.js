b446ae2a6608ae99bf6b34797719f34487cba8dc
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-04-05 13:13:45 -0500

    added social media icons, changed intro text

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,50 +11,50 @@ angular.module('garrettApp')
   .controller('MainController', function ($scope, $http) {
   	$scope.portfolio = [
   		{
-  			title: 'Project Title',
+  			title: 'RedHawk Casino',
   			image: '../images/redhawkcasino_03.jpg',
   			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus rerum, facere. Odit suscipit saepe quas nisi dolore tenetur, repellendus numquam autem vitae ducimus, aspernatur, architecto debitis animi possimus itaque laudantium.',
   			show: false
   		},
   		{
-  			title: 'Project Title',
+  			title: 'Groupon 10 Things',
   			image: '../images/tenthingsLg.jpg',
   			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet consectetur, sunt iste cupiditate dolor recusandae officia assumenda, non et officiis perspiciatis! Dolor officiis veritatis, sunt suscipit non ullam asperiores facilis!',
   			show: false
   		},
   		{
-  			title: 'Project Title',
+  			title: 'Groupon Grand Central',
   			image: '../images/grandcentralLg.jpg',
   			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore corporis cupiditate ratione repellat, aut praesentium dolorem ut ipsam quo temporibus veniam aliquid ad possimus sed unde, perferendis magni velit beatae.',
   			show: false
   		},
   		{
-  			title: 'Project Title',
+  			title: 'Inrule Development',
   			image: '../images/inrule.png',
   			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, maiores assumenda dolores omnis quisquam aut ipsum accusantium mollitia eum neque tempore ut sunt nobis nostrum maxime nesciunt odit porro quos.',
   			show: false
   		},
       {
-      	title: 'Project Title',
+      	title: 'Northshore Development',
         image: '../images/northshore.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aspernatur pariatur, tenetur repellat voluptas? Nobis, id rerum ullam doloremque maiores, architecto? Illo nulla accusamus aspernatur officiis necessitatibus facilis facere praesentium.',
         show: false
       },
   		{
-  			title: 'Project Title',
-  			image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+  			title: 'Groupon Getaways',
+  			image: '../images/merchgetaways2.jpg',
   			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quod, obcaecati doloremque nobis. Nihil porro odit libero, temporibus unde ipsum culpa veritatis, magni voluptatibus, impedit, necessitatibus ratione nisi inventore est.',
   			show: false
   		},
       {
-      	title: 'Project Title',
-        image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+      	title: 'MainStreet App',
+        image: '../images/MS_Home.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus numquam dolorem quos repellat nisi sed iste. Rem unde iste quibusdam itaque. Dolorem nesciunt vero quis deserunt ea, consectetur eligendi magnam.',
         show: false
       },
       {
-      	title: 'Project Title',
-        image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
+      	title: 'TeamMate Redesign',
+        image: '../images/teammate.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique amet debitis eligendi consequuntur maiores enim doloremque iste fugiat delectus quisquam modi nemo omnis repellat neque rem soluta, aspernatur, atque recusandae?',
         show: false
       },
