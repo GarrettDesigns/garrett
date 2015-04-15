7d041a93839c749897220dcdc50cb7775c9b841f
Garrett <garrett.kucinski@gmail.com>
2015-04-15 16:53:32 -0500

    refactored portItem, restyled intro text on main page

--- app/scripts/directives/components.js
+++ app/scripts/directives/components.js
@@ -4,6 +4,9 @@ angular.module('tmp-components',[])
 	.directive('portfolioItem', function() {
 		return {
 			restrict: 'E',
+			scope: {
+				item: '='
+			},
 			templateUrl: 'templates/portfolio-item.html',
 			replace: true
 		};
