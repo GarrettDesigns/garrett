437939abe43f1bee185c9f1d9296273f760530f1
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-05-31 18:40:51 -0500

    fixed images on details to repeat

--- app/scripts/services/portDetailService.js
+++ app/scripts/services/portDetailService.js
@@ -26,7 +26,8 @@ angular.module('garrettApp')
         description: 'Designed for Groupon, Inc. as a city guide that served as a vehicle to sell deals.',
         details: {
           title: 'Groupon 10 Things',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/tenthings-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -42,7 +43,8 @@ angular.module('garrettApp')
         description: 'The purpose of this design was to provide a visual context for the presentation of Editorial ideas about new site content.',
         details: {
           title: 'Groupon Grand Central',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -58,7 +60,8 @@ angular.module('garrettApp')
         description: 'A site done for Adage Technologies developing the front end, using responsive design techniques, CSS preprocessing and jQuery.',
         details: {
           title: 'Inrule Development',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -74,7 +77,8 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aspernatur pariatur, tenetur repellat voluptas? Nobis, id rerum ullam doloremque maiores, architecto? Illo nulla accusamus aspernatur officiis necessitatibus facilis facere praesentium.',
         details: {
           title: 'Northshore Development',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -87,10 +91,12 @@ angular.module('garrettApp')
       {
         urlTitle: 'Groupon_Getaways',
         image: '../images/merchgetaways2.jpg',
+        largeView: '../images/details/redhawkcasino_Lg.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quod, obcaecati doloremque nobis. Nihil porro odit libero, temporibus unde ipsum culpa veritatis, magni voluptatibus, impedit, necessitatibus ratione nisi inventore est.',
         details: {
           title: 'Groupon Getaways',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -106,7 +112,8 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus numquam dolorem quos repellat nisi sed iste. Rem unde iste quibusdam itaque. Dolorem nesciunt vero quis deserunt ea, consectetur eligendi magnam.',
         details: {
           title: 'MainStreet App',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -122,7 +129,8 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique amet debitis eligendi consequuntur maiores enim doloremque iste fugiat delectus quisquam modi nemo omnis repellat neque rem soluta, aspernatur, atque recusandae?',
         details: {
           title: 'TeamMate Redesign',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
             imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
             imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
@@ -135,15 +143,16 @@ angular.module('garrettApp')
       {
         urlTitle: 'Project_Title',
         image: 'http://dummyimage.com/1200x1200/4d494d/686a82.gif&text=placeholder+image',
-        detailViews: {
-          imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
-          imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
-          imageThree: '//placehold.it/600x600/f3f3f3/cccccc.png'
-        },
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga dolor est obcaecati maxime animi distinctio quia quo recusandae magni earum incidunt, quibusdam repellat nobis. Minus delectus corporis culpa eos accusamus.',
         details: {
           title: 'Project Title',
-          image: '../images/details/redhawkcasino_Lg.jpg',
+          image: '../images/headers/redhawk-header.jpg',
+          largeView: '../images/details/redhawkcasino_Lg.jpg',
+          detailViews: {
+            imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
+            imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png',
+            imageThree: '//placehold.it/600x600/f3f3f3/cccccc.png'
+          },
           description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
         },
         show: false
@@ -152,7 +161,6 @@ angular.module('garrettApp')
 
     this.get = function (title) {
       for(var i = 0; i < portfolio.length; i++) {
-        console.log(i);
         if(portfolio[i].urlTitle === title) {
           var portItem = portfolio[i];
           return portItem;
