7a1746b6da331379196917c3ed7924badf5a97ce
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-06-26 14:32:50 -0500

    got rid of large view template, added in view site button, hid view site button if no external site exists

--- app/scripts/services/portDetailService.js
+++ app/scripts/services/portDetailService.js
@@ -9,6 +9,7 @@ angular.module('garrettApp')
         description: 'A site done for Adage Technologies developing the front end, using responsive design techniques, CSS preprocessing and jQuery.',
         details: {
           title: 'Inrule Development',
+          siteUrl: 'http://www.inrule.com',
           image: '../images/headers/inrule-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
@@ -25,6 +26,7 @@ angular.module('garrettApp')
         description: 'Northshore Health Systems\' corporate website, partcipated in the redesign and development of the responsive framework',
         details: {
           title: 'Northshore Development',
+          siteUrl: 'http://www.northshore.org',
           image: '../images/headers/northshore-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
@@ -41,6 +43,7 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
         details: {
           title: 'Community Care Partners',
+          siteUrl: 'http://http://www.northshore.org/community-care-partners/',
           image: '../images/headers/community-care-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
@@ -57,6 +60,7 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique amet debitis eligendi consequuntur maiores enim doloremque iste fugiat delectus quisquam',
         details: {
           title: 'TeamMate Redesign',
+          siteUrl: '',
           image: '../images/headers/teammate-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
@@ -73,11 +77,11 @@ angular.module('garrettApp')
         description: 'An exciting redesign of RedHawk Casino\'s main web page. This project was done in a bid to win this client for Adage Technologies.',
         details: {
           title: 'RedHawk Casino',
+          siteUrl: '',
           image: '../images/headers/redhawk-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
-            imageOne: '../images/details/redhawkcasino_head.jpg',
-            imageTwo: '../images/details/redhawkcasino_footer.jpg'
+            imageOne: '../images/details/redhawkcasino_Lg.jpg'
           },
           overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
         },
@@ -89,11 +93,11 @@ angular.module('garrettApp')
         description: 'Designed for Groupon, Inc. as a city guide that served as a vehicle to sell deals.',
         details: {
           title: 'Groupon 10 Things',
+          siteUrl: '',
           image: '../images/headers/tenthings-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
-            imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
-            imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png'
+            imageOne: '../images/details/merchtenthings2.jpg'
           },
           overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
         },
@@ -105,6 +109,7 @@ angular.module('garrettApp')
         description: 'The purpose of this design was to provide a visual context for the presentation of Editorial ideas about new site content.',
         details: {
           title: 'Groupon Grand Central',
+          siteUrl: '',
           image: '../images/headers/grand-central-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
@@ -121,6 +126,7 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quod, obcaecati doloremque nobis. Nihil porro odit libero, temporibus unde ipsum culpa veritatis, magni voluptatibus, impedit, necessitatibus ratione nisi inventore est.',
         details: {
           title: 'Groupon Getaways',
+          siteUrl: '',
           image: '../images/headers/merchant-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
@@ -136,6 +142,7 @@ angular.module('garrettApp')
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus numquam dolorem quos repellat nisi sed iste. Rem unde iste quibusdam itaque. Dolorem nesciunt vero quis deserunt ea, consectetur eligendi magnam.',
         details: {
           title: 'MainStreet App',
+          siteUrl: '',
           image: '../images/headers/mainstreet-header.jpg',
           largeView: '../images/details/redhawkcasino_Lg.jpg',
           detailViews: {
