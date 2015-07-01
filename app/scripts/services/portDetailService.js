642694a157e75ba17dc3a17e324b95e9fbb17119
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-06-30 20:17:31 -0500

    added bso, took out mainstreet app, wrote some descriptions

--- app/scripts/services/portDetailService.js
+++ app/scripts/services/portDetailService.js
@@ -15,14 +15,14 @@ angular.module('garrettApp')
             imageOne: 'images/details/inrule_detail.png',
             imageTwo: 'images/details/inrule_detail2.png'
           },
-          overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
+          overview: 'InRule was designed by an outside contract firm and brought to Adage Technologies to complete the responsive development and CMS implementation. As the site was developed I advised the company on some design alterations that would better suit their site as I developed the responsive framework. This site was developed using Foundation and Sass and is fully responsive.'
           },
         show: false
       },
       {
         urlTitle: 'Northshore_Development',
         image: '../images/gallery/northshore.png',
-        description: 'Northshore Health Systems\' corporate website, partcipated in the redesign and development of the responsive framework',
+        description: 'Northshore Health Systems\' corporate website, I partcipated in the redesign and development of the site and responsive framework.',
         details: {
           title: 'Northshore Development',
           siteUrl: 'http://www.northshore.org',
@@ -31,7 +31,7 @@ angular.module('garrettApp')
             imageOne: 'images/details/northshore_full.jpg',
             imageTwo: 'images/details/ns_shot.jpg'
           },
-          overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
+          overview: 'As a member of the Adage Design and Development team I participated in the redesign and development of Northshore Health System\'s new website. This site is built using Foundation 4 as well as incorporating Sass and a backend CMS built on EpiServer.'
         },
         show: false
       },
@@ -52,16 +52,30 @@ angular.module('garrettApp')
         show: false
       },
       {
-        urlTitle: 'TeamMate_Redesign',
-        image: '../images/gallery/teammate.png',
+        urlTitle: 'Cleveland Orchestra',
+        image: '../images/gallery/cleveland.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique amet debitis eligendi consequuntur maiores enim doloremque iste fugiat delectus quisquam',
         details: {
-          title: 'TeamMate Redesign',
-          siteUrl: '',
-          image: '../images/headers/teammate-header.jpg',
+          title: 'Cleveland Orchestra',
+          siteUrl: 'http://www.clevelandorchestra.com/',
+          image: '../images/headers/cleveland-header.jpg',
           detailViews: {
-            imageOne: '//placehold.it/600x600/f3f3f3/cccccc.png',
-            imageTwo: '//placehold.it/600x600/f3f3f3/cccccc.png'
+            imageOne: 'http://placehold.it/600x600'
+          },
+          overview: 'Though it had been recently redesigned and relaunched by another firm, The Cleveland Orchestra wasn’t happy with the design or performance of its website. The look and feel of the site was old and stale. The purchase path for tickets was treacherous, and more often than not, spurred calls to the box office from angry and confused patrons. Adage built the Orchestra a responsive website that allows users to browse rich content and buy tickets for performances at the group’s Severance Hall home and its summer events at Blossom Music Festival.'
+        },
+        show: false
+      },
+      {
+        urlTitle: 'boston-symphony',
+        image: '../images/gallery/bso.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus numquam dolorem quos repellat nisi sed iste. Rem unde iste quibusdam itaque. Dolorem nesciunt vero quis deserunt ea, consectetur eligendi magnam.',
+        details: {
+          title: 'BSO Archives',
+          siteUrl: 'http://archives.bso.org/',
+          image: '../images/headers/bso-header.jpg',
+          detailViews: {
+            imageOne: '../images/details/bso-detail.jpg'
           },
           overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
         },
@@ -122,22 +136,6 @@ angular.module('garrettApp')
           image: '../images/headers/merchant-header.jpg',
           detailViews: {
             imageOne: 'images/details/getaways_detail.jpg'
-          },
-          overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
-        },
-        show: false
-      },
-      {
-        urlTitle: 'MainStreet_App',
-        image: '../images/gallery/MS_Home.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus numquam dolorem quos repellat nisi sed iste. Rem unde iste quibusdam itaque. Dolorem nesciunt vero quis deserunt ea, consectetur eligendi magnam.',
-        details: {
-          title: 'MainStreet App',
-          siteUrl: '',
-          image: '../images/headers/mainstreet-header.jpg',
-          detailViews: {
-            imageOne: '../images/details/MS_detail.jpg',
-            imageTwo: '../images/details/MS_rewards.jpg'
           },
           overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum labore quibusdam quia nulla possimus, nemo expedita officiis nihil architecto dolorem soluta assumenda totam ad odio sit, vel, sint et ratione.'
         },
